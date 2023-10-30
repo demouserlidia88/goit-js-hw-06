3ed3502e0b7bfb9bb43ffe5edf2a8e207839aeb0
demouserlidia88 <lidia.molodiuc@gmail.com>
2023-10-30 12:25:15 +0200

    Al-patrulea commit
    task-8,9,10

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+	event.preventDefault();
+	const emailInput = loginForm.elements.email;
+	const passwordInput = loginForm.elements.password;
+	if (emailInput.value === "" || passwordInput === "") {
+		alert("Please fill in all the fields!");
+	} else {
+		const formData = {
+			email: emailInput.value,
+			password: passwordInput.value,
+		};
+		console.log("The data entered:", formData);
+	}
+	loginForm.reset();
+}
 
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,35 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+const boxesDiv = document.getElementById("boxes");
+function createBoxes(amount) {
+	for (let i = 1; i <= amount; i++) {
+		const box = document.createElement("div");
+		box.style.width = `${30 + i * 10}px`;
+		box.style.height = `${30 + i * 10}px`;
+		box.style.backgroundColor = getRandomHexColor();
+		boxesDiv.appendChild(box);
+	}
+}
+function destroyBoxes(amount) {
+	const boxes = boxesDiv.querySelectorAll("div");
+	const removeCount = Math.min(amount, boxes.length);
+	for (let i = boxes.length - 1; i >= boxes.length - removeCount; i--) {
+		boxesDiv.removeChild(boxes[i]);
+	}
+}
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+createButton.addEventListener("click", () => {
+	const input = document.querySelector("input");
+	const inputVal = parseInt(input.value);
+	createBoxes(inputVal);
+});
+destroyButton.addEventListener("click", () => {
+	const input = document.querySelector("input");
+	const inputVal = parseInt(input.value);
+	destroyBoxes(inputVal);
+});
+
